7c18b65ad5c46ebccc2fa2a9c972bff2549c15d6
Sohag Rana <sohagrana921@gmail.com>
2023-04-10 00:36:43 +0600

    header sec done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="my-container">
+      <nav className="flex justify-center items-center">
+        <Link className="text-2xl font-extrabold mr-auto">CareerHub</Link>
+        <Link className="mr-10 font-bold">Statistics</Link>
+        <Link className="mr-10 font-bold">Applied Jobs</Link>
+        <Link className="font-bold">Blog</Link>
+        <Link className="btn ml-auto">Start Applying</Link>
+      </nav>
+      <Banner></Banner>
     </div>
   );
 };
